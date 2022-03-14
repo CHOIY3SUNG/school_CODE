cd742651511769cd6709288e39c132cfc6079e54
CHOIYESUNG <choiyesung2848@gmail.com>
2022-03-14 13:11:52 +0900

    자료형

--- First.java
+++ First.java
@@ -1,4 +1,9 @@
-public class first {
+//   파일이름 : first.java
+//   작성자 : 최예성
+//   작성일 : 2022.03.14 (화이트 데이..)
+//   작성이유 : 선생님이 쓰래서
+
+public class First {
     public static void main(String[] args){
         System.out.println(7);
         System.out.println(3.15);
