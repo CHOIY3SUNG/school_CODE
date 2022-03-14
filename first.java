5744200707bf3a869d61edfbb53f07f8b722b245
CHOIYESUNG <choiyesung2848@gmail.com>
2022-03-14 11:28:39 +0900

    println

--- first.java
+++ first.java
@@ -1,5 +1,10 @@
 public class first {
     public static void main(String[] args){
-        System.out.println("hello world");
+        System.out.println(7);
+        System.out.println(3.15);
+        System.out.println("3 + 5 = " + 8);
+        System.out.println(3.15 + "는 실수입니다.");
+        System.out.println("3 + 5" + "의 연산 결가는 8입니다.");
+        System.out.println(3 + 5);  // 덧셈 결과 출력
     }
 }
