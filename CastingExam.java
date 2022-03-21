b0488bdeeb1f318b1ea035f4041cd3edd862e3ef
CHOIYESUNG <choiyesung2848@gmail.com>
2022-03-21 13:15:01 +0900

    byte_int

--- CastingExam.java
+++ CastingExam.java
@@ -1,8 +1,12 @@
 public class CastingExam {
     public static void main(String[] args) {
         byte a = 10;
-        int b = a;      // int b = (int)a; 자동이므로 생략
+        int b = a;
         System.out.println(a);
         System.out.println(b);
+        System.out.println("-----");
+
+//      int c = 20;
+//      byte d = c;     // 크기가 맞지 않아 에러가 난다.
     }
 }
