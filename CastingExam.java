3e0306cbe4ad5bee96773d5fb22bcfbc25387fa9
CHOIYESUNG <choiyesung2848@gmail.com>
2022-03-23 13:44:07 +0900

    CastingExam

--- CastingExam.java
+++ CastingExam.java
@@ -5,7 +5,6 @@ public class CastingExam {
         System.out.println(a);
         System.out.println(b);
         System.out.println("-----");
-
 //      int c = 20;
 //      byte d = c;     // 크기가 맞지 않아 에러가 난다.
     }
